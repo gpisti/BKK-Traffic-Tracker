fb98d1e70873e9b0843365ee67bda48cdfe25382
gpisti <113467025+gpisti@users.noreply.github.com>
2025-01-03 20:36:02 +0100

    Create config.py
    Censured my API Key, uploaded the missing config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+API_KEY = "Request an API Key for testing at the following link: https://opendata.bkk.hu/keys"
+
+KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
+KAFKA_TOPIC_NAME = "vehicle-data"
+
+DB_NAME = "bkk_traffic_tracker"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_HOST = "localhost"
+DB_PORT = "5432"
